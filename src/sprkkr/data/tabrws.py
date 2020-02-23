2cc6e7b860a673a35ce7805e1e0306a986855734
Jan Minar <jminar@ntc.zcu.cz>
2020-02-23 17:17:32 +0100

    Table of RWS from Skrives book

---
+++ src/sprkkr/data/tabrws.py
@@ -0,0 +1,58 @@
+# coding: utf-8
+# vim: set et ts=4 sw=4 nu fdm=indent cc=80:
+
+"""
+Module tabrws
+===============
+
+This module contains the Wigner-Seitz radii for elements in Bohr units
+        return the Wigner-Seitz-radii RWS for atomic number  Z 
+        experimental values taken from H.L. Skrivers LMTO book       
+
+
+.. code-block:: python
+
+    from sprkkr.data import tabrws
+    print(tabrws[3]]=
+    3.25
+
+"""
+tabrws = [None for i in range(105)]
+
+tabrws[0]=    0.000 ; tabrws[1]=    0.000 ; tabrws[2]=    0.000
+tabrws[3]=    0.000 ; tabrws[4]=    0.000 ; tabrws[5]=    0.000
+tabrws[6]=    0.000 ; tabrws[7]=    0.000 ; tabrws[8]=    0.000
+tabrws[9]=    0.000 ; tabrws[10]=   0.000 ; tabrws[11]=   0.000
+tabrws[12]=   0.000 ; tabrws[13]=   0.000 ; tabrws[14]=   0.000
+tabrws[15]=   0.000 ; tabrws[16]=   0.000 ; tabrws[17]=   0.000
+tabrws[18]=   0.000 ; tabrws[19]=   4.862 ; tabrws[20]=   4.122
+tabrws[21]=   3.427 ; tabrws[22]=   3.052 ; tabrws[23]=   2.818
+tabrws[24]=   2.684 ; tabrws[25]=   2.699 ; tabrws[26]=   2.662
+tabrws[27]=   2.621 ; tabrws[28]=   2.602 ; tabrws[29]=   2.669
+tabrws[30]=   0.000 ; tabrws[31]=   0.000 ; tabrws[32]=   0.000
+tabrws[33]=   0.000 ; tabrws[34]=   0.000 ; tabrws[35]=   0.000
+tabrws[36]=   0.000 ; tabrws[37]=   5.197 ; tabrws[38]=   4.494
+tabrws[39]=   3.761 ; tabrws[40]=   3.347 ; tabrws[41]=   3.071
+tabrws[42]=   2.922 ; tabrws[43]=   2.840 ; tabrws[44]=   2.791
+tabrws[45]=   2.809 ; tabrws[46]=   2.873 ; tabrws[47]=   3.005
+tabrws[48]=   0.000 ; tabrws[49]=   0.000 ; tabrws[50]=   0.000
+tabrws[51]=   0.000 ; tabrws[52]=   0.000 ; tabrws[53]=   0.000
+tabrws[54]=   0.000 ; tabrws[55]=   5.656 ; tabrws[56]=   4.652
+tabrws[57]=   3.920 ; tabrws[58]=   3.800 ; tabrws[59]=   3.818
+tabrws[60]=   3.804 ; tabrws[61]=   3.783 ; tabrws[62]=   3.768
+tabrws[63]=   4.263 ; tabrws[64]=   3.764 ; tabrws[65]=   3.720
+tabrws[66]=   3.704 ; tabrws[67]=   3.687 ; tabrws[68]=   3.668
+tabrws[69]=   3.649 ; tabrws[70]=   4.052 ; tabrws[71]=   3.624
+tabrws[72]=   3.301 ; tabrws[73]=   3.069 ; tabrws[74]=   2.945
+tabrws[75]=   2.872 ; tabrws[76]=   2.825 ; tabrws[77]=   2.835
+tabrws[78]=   2.897 ; tabrws[79]=   3.002 ; tabrws[80]=   0.000
+tabrws[81]=   0.000 ; tabrws[82]=   0.000 ; tabrws[83]=   0.000
+tabrws[84]=   0.000 ; tabrws[85]=   0.000 ; tabrws[86]=   0.000
+tabrws[87]=   5.900 ; tabrws[88]=   4.790 ; tabrws[89]=   3.900
+tabrws[90]=   3.756 ; tabrws[91]=   3.430 ; tabrws[92]=   3.221
+tabrws[93]=   3.140 ; tabrws[94]=   3.181 ; tabrws[95]=   3.614
+tabrws[96]=   3.641 ; tabrws[97]=   3.550 ; tabrws[98]=   0.000
+tabrws[99]=   0.000 ; tabrws[100]=  0.000 ; tabrws[101]=  0.000
+tabrws[102]=  0.000 ; tabrws[103]=  3.500 ; tabrws[104]=  0.000
+
+
