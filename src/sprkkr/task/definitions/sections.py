976fcb3cfa5879710f74223e4f4801a811643e10
Matyas Novak <logik@centrum.cz>
2021-04-03 15:07:53 +0200

    Task sections - some fixies of their definitions to be readable by SPR-KRR

--- src/sprkkr/task/definitions/sections.py
+++ src/sprkkr/task/definitions/sections.py
@@ -1,19 +1,19 @@
 def _sections():
-  from ...common.grammar_types  import Keyword, SetOf, Flag, energy
+  from ...common.grammar_types  import Keyword, DefKeyword, SetOf, Flag, energy
   from ..task_definitions import \
       SectionDefinition as Section, \
       ValueDefinition as V
 
   sections = {}
   sections['CONTROL'] = lambda x: Section('CONTROL',[
-      V('ADSI', Keyword(x), required = True, help="Type of the computation -- do DFT selfconsistent cycle"),
-      V('DATASET', str, required = True, help="Meaning of the parameter"),
-      V('POTFIL', str, required=True, help="Potential file (see SPRKKR documentation for its format)"),
+      V('DATASET', str, 'case', required = True, help="Meaning of the parameter"),
+      V('ADSI', DefKeyword(x), required = True, help="Type of the computation -- do DFT selfconsistent cycle"),
+      V('POTFIL', str, required=True, help="Potential file (see SPRKKR documentation for its format). It is not necessary to set it, it will be set by the calculator."),
       V('KRWS', int)
   ])
 
   sections['TAU'] = Section('TAU',[
-      V('BZINT', Keyword('POINTS', 'ANOTHER_OPTION'), required=True),
+      V('BZINT', DefKeyword('POINTS', 'ANOTHER_OPTION'), required=True),
       V('NKTAB', 250)
   ])
 
@@ -27,7 +27,7 @@ def _sections():
   sections['SCF'] = Section('SCF', [
       V('NITER', 200),
       V('MIX', 0.2),
-      V('VXC', Keyword('VWN')),
+      V('VXC', DefKeyword('VWN')),
       V('EFUESS', 0.7),
       V('TOL', 0.00001),
       V('ISTBRY', 1)
@@ -38,7 +38,7 @@ def _sections():
   ])
 
   sections['TASK'] = lambda x: Section('TASK', [
-    V('TASK', Keyword(x))
+    V('TASK', DefKeyword(x))
   ])
 
   return sections
