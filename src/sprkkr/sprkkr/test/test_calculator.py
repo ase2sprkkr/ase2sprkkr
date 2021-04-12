3af7641af01f071ccc6ea833d9ccf4ffe0419bf8
Matyas Novak <logik@centrum.cz>
2021-04-12 15:47:38 +0200

    More test for task running

--- src/sprkkr/sprkkr/test/test_calculator.py
+++ src/sprkkr/sprkkr/test/test_calculator.py
@@ -15,25 +15,22 @@ from ...calcio import PotFile
 class CalculatorTest(TestCase):
 
   def test_calculator(self):
-      #atoms = bulk('Li')
-      #atoms.cell = atoms.cell / 2 #lwoer computational requirements
-      #pf = PotFile(atoms=atoms,filename = 'output2_test_calc.pot', directory = os.path.dirname(__file__))
-      #pf.write()
-      #import subprocess
-      #subprocess.run("kkrscf < output2_test_calc.inp", shell=True)
      print_output = '-v' in sys.argv or '--verbose' in sys.argv
+
+     #out = SprKkr.Potential.from_file(os.path.dirname(__file__) + '/output_test_calc.pot_new')
 
      atoms = bulk('Li')
      calculator = SprKkr(atoms = atoms, mpi=False, directory = os.path.dirname(__file__), input_file = 'output_test_calc.inp', output_file = 'output_test_calc.out', potential_file ='output_test_calc.pot')
      out = calculator.calculate(options = {'NITER' : 2 }, print_output=print_output)
-     self.assertEquals(2, len(out))
+     self.assertEqual(2, len(out.iterations))
+     self.assertEqual(str(atoms.symbols), str(out.atoms.symbols))
+     self.assertFalse(out.converged)
 
      atoms=bulk('LiCl', 'rocksalt', a=5.64) * (2, 1, 1)
      calculator = SprKkr(atoms = atoms, mpi=False, directory = os.path.dirname(__file__), input_file = 'output_test_calc.inp', output_file = 'output_test_calc.out', potential_file ='output_test_calc.pot')
      out = calculator.calculate(options = {'NITER' : 1, 'NE' : 12 }, print_output=print_output)
-     self.assertEquals(1, len(out))
-     self.assertEquals(3, len(out[-1]['atoms']))
-     for i in out[-1]['atoms']:
-        self.assertEquals(5, len(i['orbitals']))
-
-
+     self.assertEqual(1, len(out.iterations))
+     self.assertEqual(3, len(out.iterations[-1]['atoms']))
+     for i in out.iterations[-1]['atoms']:
+        self.assertEqual(5, len(i['orbitals']))
+     self.assertEqual(str(atoms.symbols), str(out.atoms.symbols))
