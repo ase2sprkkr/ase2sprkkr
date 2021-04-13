20fbbd9713a49ebe28d3746d86cca89c3f42e4c8
Matyas Novak <logik@centrum.cz>
2021-04-13 11:51:39 +0200

    Test of plotting and some fixies of the test

--- src/sprkkr/sprkkr/test/test_calculator.py
+++ src/sprkkr/sprkkr/test/test_calculator.py
@@ -14,22 +14,23 @@ class CalculatorTest(TestCase):
   def test_calculator(self):
      print_output = '-v' in sys.argv or '--verbose' in sys.argv
 
-     #out = SprKkr.Potential.from_file(os.path.dirname(__file__) + '/output_test_calc.pot_new')
+     dirname = os.path.dirname(__file__)
+     here = lambda x: os.path.join(dirname, x)
 
      atoms = bulk('Li')
-     calculator = SprKkr(atoms = atoms, mpi=False, directory = os.path.dirname(__file__), input_file = 'output_test_calc.inp', output_file = 'output_test_calc.out', potential_file ='output_test_calc.pot')
+     calculator = SprKkr(atoms = atoms, mpi=False, directory = dirname, input_file = 'output_test_calc.inp', output_file = 'output_test_calc.out', potential_file ='output_test_calc.pot')
      out = calculator.calculate(options = {'NITER' : 2 }, print_output=print_output)
      self.assertEqual(2, len(out.iterations))
      self.assertEqual(str(atoms.symbols), str(out.atoms.symbols))
      self.assertFalse(out.converged)
 
      #read again the output from a file - the results should be the same
-     SprKkr.Task.create('scf').read_output_from_file(os.path.join(os.path.dirname(__file__), 'output_test_calc.out'))
-     self.assertEqual(str(atoms.symbols), str(out.atoms.symbols))
+     out = SprKkr.Task.create('scf').read_output_from_file(here('output_test_calc.out'))
      self.assertEqual(2, len(out.iterations))
+     out.plot(filename = here('output_test_calc.png'))
 
      atoms=bulk('LiCl', 'rocksalt', a=5.64) * (2, 1, 1)
-     calculator = SprKkr(atoms = atoms, mpi=False, directory = os.path.dirname(__file__), input_file = 'output_test_calc.inp', output_file = 'output_test_calc.out', potential_file ='output_test_calc.pot')
+     calculator = SprKkr(atoms = atoms, mpi=False, directory = dirname, input_file = 'output_test_calc.inp', output_file = 'output_test_calc.out', potential_file ='output_test_calc.pot')
      out = calculator.calculate(options = {'NITER' : 1, 'NE' : 12 }, print_output=print_output)
      self.assertEqual(1, len(out.iterations))
      self.assertEqual(3, len(out.iterations[-1]['atoms']))
@@ -37,12 +38,12 @@ class CalculatorTest(TestCase):
         self.assertEqual(5, len(i['orbitals']))
      self.assertEqual(str(atoms.symbols), str(out.atoms.symbols))
 
-     SprKkr.Task.create('scf').read_output_from_file(os.path.join(os.path.dirname(__file__), 'output_test_calc.out'))
+     out = SprKkr.Task.from_file(here('output_test_calc.inp')).read_output_from_file(here('output_test_calc.out'))
      self.assertEqual(str(atoms.symbols), str(out.atoms.symbols))
      self.assertEqual(1, len(out.iterations))
 
      atoms = bulk('Li')
-     calculator = SprKkr(atoms = atoms, mpi=False, directory = os.path.dirname(__file__), input_file = 'output_test_calc.inp', output_file = 'output_test_calc.out', potential_file ='output_test_calc.pot')
+     calculator = SprKkr(atoms = atoms, mpi=False, directory = dirname, input_file = 'output_test_calc.inp', output_file = 'output_test_calc.out', potential_file ='output_test_calc.pot')
      task = SprKkr.Task.create('scf')
      task.SCF.NITER = 1
      out = calculator.calculate(task = task, print_output=print_output)
@@ -51,7 +52,7 @@ class CalculatorTest(TestCase):
      self.assertEqual(str(atoms.symbols), str(out.atoms.symbols))
      self.assertFalse(out.converged)
 
-     calculator = SprKkr(mpi=False, directory = os.path.dirname(__file__), input_file = 'output_test_calc.inp', output_file = 'output_test_calc.out', potential_file ='output_test_calc.pot')
+     calculator = SprKkr(mpi=False, directory = dirname, input_file = 'output_test_calc.inp', output_file = 'output_test_calc.out', potential_file ='output_test_calc.pot')
      out = calculator.calculate(potential = out.potential, task = task)
      self.assertEqual(str(atoms.symbols), str(out.atoms.symbols))
      self.assertEqual(1, len(out.iterations))
