6a7f497b77881e57d74d36ab32d11bd4ca8df7ca
Matyas Novak <logik@centrum.cz>
2021-05-19 17:06:21 +0200

    calculator.save_input method and better filenames handling

--- src/sprkkr/sprkkr/calculator.py
+++ src/sprkkr/sprkkr/calculator.py
@@ -105,7 +105,7 @@ class SprKkr(Calculator):
 
         self.input_file = (self.label or '') + 'a_%t.inp' if input_file is True else input_file
         self.output_file = output_file
-        self.potential_file = (self.label or '') + '%a.pot' if potential_file is True else potential_file
+        self.potential_file = potential_file
         self.task = Task.create_task(task)
 
         """ For %c template in file names """
@@ -147,7 +147,8 @@ class SprKkr(Calculator):
         self._counter+=1
         return self.counter
 
-    def _open_file(self, filename, templator=None, named=False, mode='w+'):
+    def _open_file(self, filename, templator=None, named=False, mode='w+',
+                         allow_temporary=True, create_subdirs=False):
         """
         Open a file given a 'template' filename file name
 
@@ -167,9 +168,18 @@ class SprKkr(Calculator):
 
         mode: str
           Mode to open the file
+
+        allow_temporary: bool
+          If False, throw an exception if the resulting filename should be temporary,
+          i.e. if None as filename is given
+
+        create_subdirs: bool
+          If true, create all non-existent directories in the given path
         """
 
         if filename is None:
+           if not allow_temporary:
+              raise ValueException("Creation of temporary files is not allowed in this context")
            f = tempfile.NamedTemporaryFile if named else tempfile.TemporaryFile
            return f(mode = mode)
         if not isinstance(filename, str):
@@ -181,12 +191,196 @@ class SprKkr(Calculator):
         if '/' not in filename and self._directory:
            filename = os.path.join(self._directory, filename)
 
+        if create_subdirs:
+           dirs = os.path.dirname(filename)
+           try:
+             os.makedirs(dirs)
+           except FileExistsError:
+             pass
+
         return open(filename, mode)
+
+    def save_input(self, atoms=None, task=None, system_changes=all_changes,
+                  potential=None, task_file=None, potential_file=None, output_file=False,
+                  create_subdirs=False,
+                  options = {}, return_files=False):
+        """
+        Save input for calculations
+
+        Paramters
+        ---------
+        properties: list of str
+            List of what needs to be calculated.  Can be any combination
+            of 'energy', 'forces', 'stress', 'dipole', 'charges', 'magmom'
+            and 'magmoms'.
+
+        task: sprkkr.task.tasks.Task or str or None
+            If None, task specified in __init__ is used, or the default 'SCF' task
+               will be created.
+            If string is given then if it is a task name, the Task object will be created
+               and the task file created using it.
+               Otherwise the task is interpreted as task file, which will be left unchanged.
+            The task is written into the location given by self.input_file.
+
+        system_changes: list of str
+            List of what has changed since last calculation.  Can be
+            any combination of these six: 'positions', 'numbers', 'cell',
+            'pbc', 'initial_charges' and 'initial_magmoms'.
+
+            This calculator ignore properties and system_changes.
+
+        potential: sprkkr.potential.potentials.Potential or str or None or False
+            If False, the potential file is specified in the task.
+            If None, create the potential according to the atoms and self.potential_file property.
+            If str, then it is the filename of the potential, that is left unchanged on input
+                (however, it will be modified by SPRKKR itself)
+
+        task_file: str or None
+            Filename or template (see FilenameTemplator class) where to save the task file.
+            None means to use the default value from the Calculator.
+
+        potential_file: str or None
+            Filename or template (see FilenameTemplator class) where to save the potential_file
+            None means to use the default value from the Calculator.
+            If potential is given as filename, the potential is NOT readed and stored again,
+            so in this case setting potential_file has no effect.
+
+        output_file: str or None or False
+             Filename or template (see FilenameTemplator class) where to save the output
+             None means to use the default value from the Calculator.
+             False means not to create the file.
+
+        create_subdirs: boolean
+             If true, create directories if they don't exists.
+
+        options: dict
+            Options to set to the task. If task is given by filename, it is readed, altered by the
+            options and then the modified task (in a temporary file) will be used.
+
+        return_files: boolean
+            Return open files object instead of just string filenames.
+
+        Returns
+        -------
+        task: Task
+            Created task object to be run.
+
+        task_filename: str or file
+            Input/task file. See return_files
+
+        potential_filename: str or file
+            Potential file. See return_files
+
+        output_filename: str or file
+            Output file. See return_files
+        """
+
+        def makepath(path, must_exist):
+           if '/' not in path and self._directory:
+              path = os.path.join(self._directory, path)
+           if must_exist and not os.path.isfile(task):
+              raise ValueError(must_exist.format(path))
+           return path
+
+        def from_task_name(template, replacement, default):
+            if template is not True:
+               return template
+            if task_file.name:
+               if task_file.name.endswith('.inp'):
+                  return task_file.name[:-4] + replacement
+               if task_file.name.endswith('.in'):
+                  return task_file.name[:-3] + replacement
+            return default
+
+        templator = FilenameTemplator(self)
+
+        """ Get the task file """
+        if task:
+           if isinstance(task, str):
+              if Task.is_it_a_task_name(task):
+                 task = task.create_task(task)
+              else:
+                 task_file = makepath(task, "'{path}' is not a task file nor a known name of task.")
+                 if options or potname:
+                    task = task.from_file(task_file)
+                 else:
+                    task = None
+        else:
+           task = self.task or Task.default_task()
+
+        task_file = self._open_file(task_file or self.input_file, templator, False,
+                                    allow_temporary=return_files,
+                                    create_subdirs=create_subdirs
+                                    )
+
+        """ Get the potential file """
+        # potential - potntial object
+        if potential:
+           if isinstance(potential, str):
+              potential_file = makepath(potential, "'{path}' is not a potential file")
+              potential = None
+              potname = potential_file
+        elif potential is not False:
+           potential = Potential.from_atoms(self.atoms)
+        else:
+           potname = potential_file = None
+
+        if potential:
+           pf = potential_file or self.potential_file
+           pf = from_task_name(pf, '.pot', '%a.pot')
+           potential_file = self._open_file(pf, templator, True,
+                                            allow_temporary=return_files,
+                                            create_subdirs=create_subdirs)
+           potential.save_to_file(potential_file, self.atoms)
+           potential_file.close()
+           potname = potential_file.name
+
+        """ update task by the potential """
+        if task:
+          if options:
+            task.set(options, unknown = 'find')
+          if potname:
+            #use the relative potential file name to avoid too-long-
+            #-potential-file-name problem
+            dirr = os.path.dirname( task_file.name ) if task_file.name else self.directory
+            task.CONTROL.POTFIL = os.path.relpath( potname, dirr )
+            task.save_to_file(task_file)
+            task_file.seek(0)
+        #This branch can occures only if the potential have been explicitly set to False,
+        #which means not to create the potential (and take it from the task file)
+        else:
+            task = task.from_file(task_file)
+            potname = task.CONTROL.POTFIL
+
+        if output_file is not False:
+          """ Get the output file """
+          output_file = output_file or self.output_file
+          output_file = from_task_name(output_file, '.out', '%a_%t.out')
+          output_file = self._open_file(output_file, templator, False, mode='wb',
+                                            allow_temporary=return_files,
+                                            create_subdirs=create_subdirs)
+
+        if not return_files:
+          task_file.close()
+          for f in task_file, output_file:
+              if hasattr(f,'close'):
+                 f.close()
+          task_file = task_file.name
+          if output_file:
+             output_file = output_file.name
+
+        if output_file:
+           return task, task_file, potname, output_file
+        else:
+           return task, task_file, potname
+
 
 
     def calculate(self, atoms=None, task=None, system_changes=all_changes,
-                  potential = None, output_file=False, print_output=False,
-                  command_postfix=None, options = {}):
+                  potential=None, task_file=None, potential_file=None, output_file=None,
+                  create_subdirs=False,
+                  options={},
+                  print_output=False, command_postfix=None):
         """
         Do the calculation.
 
@@ -200,120 +394,28 @@ class SprKkr(Calculator):
                             'charges': np.zeros(len(atoms)),
                             'magmom': 0.0,
                             'magmoms': np.zeros(len(atoms))}
-
-
-        Paramters
-        ---------
-        properties: list of str
-            List of what needs to be calculated.  Can be any combination
-            of 'energy', 'forces', 'stress', 'dipole', 'charges', 'magmom'
-            and 'magmoms'.
-        system_changes: list of str
-            List of what has changed since last calculation.  Can be
-            any combination of these six: 'positions', 'numbers', 'cell',
-            'pbc', 'initial_charges' and 'initial_magmoms'.
-
-            This calculator ignore properties and system_changes.a
-
-        task: sprkkr.task.tasks.Task or str or None
-            If None, task specified in __init__ is used, or the default 'SCF' task
-               will be created.
-            If string is given then if it is a task name, the Task object will be created
-               and the task file created using it.
-               Otherwise the task is interpreted as task file, which will be left unchanged.
-            The task is written into the location given by self.input_file.
-
-        potential: sprkkr.potential.potentials.Potential or str or None or False
-            If False, the potential file is specified in the task.
-            If None, create the potential according to the atoms and self.potential_file property.
-            If str, then it is the filename of the potential, that is left unchanged on input
-                (however, it will be modified by SPRKKR itself)
-
-        output_file: str or False or True or a file object
-            Filename or open file to store output of the SPRKKR. False mean do not store (just read),
-            True mean to use the default template.
+        Parameters
+        ----------
+        For other parameters, see save_input() method
 
         print_output: bool
             Print output to stdout, too.
 
         command_postfix: str or bool or None
             If not None, override command_postifx specified when the calculator have been created.
-
-        options: dict
-            Options to set to the task. If task is given by filename, it is readed, altered by the
-            options and then the modified task (in a temporary file) will be used.
-
         """
 
-        def makepath(path, must_exist):
-           if '/' not in path and self._directory:
-              path = os.path.join(self._directory, path)
-           if must_exist and not os.path.isfile(task):
-              raise ValueError(must_exist.format(path))
-           return path
+        super().calculate(atoms, system_changes)
 
-        super().calculate(atoms, task, system_changes)
+        if output_file==False:
+           output_file = None
 
-        templator = FilenameTemplator(self)
-
-        """ Get the potential file """
-        if potential:
-           if isinstance(potential, str):
-              potential_file = makepath(task, "'{path}' is not a task file nor a known name of task.")
-              potential = None
-        elif potential is not False:
-           potential = Potential.from_atoms(self.atoms)
-        if potential:
-           potential_file = self._open_file(self.potential_file, templator, True)
-           potential.save_to_file(potential_file, self.atoms)
-
-        """ Get the task file """
-        if task:
-           if isinstance(task, str):
-              if Task.is_it_a_task_name(task):
-                 task = task.create_task(task)
-              else:
-                 task_file = makepath(task, "'{path}' is not a task file nor a known name of task.")
-                 task = None
-        else:
-           task = self.task or Task.default_task()
-
-        if options and not task:
-           task = task.from_file(task)
-           task_file = None #use a temporary file
-
-        if task:
-           if options:
-               task.set(options, unknown = 'find')
-           task_file = self._open_file(self.input_file, templator, False)
-           if potential_file.name:
-              potname = potential_file.name
-              #use the relative potential file name to avoid too-long-
-              #-potential-file-name problem
-              try:
-                  common = os.path.commonpath([self.directory, potname])
-                  if common:
-                      potname = potname[len(common)+1:]
-              except ValueError:
-                 pass
-              task.CONTROL.POTFIL = potname
-           task.save_to_file(task_file)
-           task_file.seek(0)
-
-        """ Get the output file """
-        output_file = output_file or self.output_file
-        if output_file is True:
-           if getattr(task_file, 'name', None):
-              output_file = re.sub('(.in(p?))?', '.out', task_file, name)
-              output_file = open(output_file, 'wb')
-           else:
-              output_file = self._open_file('%a_%t.out', templator, False, mode='wb')
-        elif output_file:
-              output_file = self._open_file(output_file, templator, False, mode='wb')
+        task, task_file, _, output_file = self.save_input(atoms=atoms, task=task,
+                            potential=potential, output_file=output_file, options=options,
+                            return_files=True)
 
         return task.run_task_process(self, task_file, output_file, print_output,
-                                    command_postfix = command_postfix
-                             )
+                                    command_postfix = command_postfix)
 
 
     def phagen(self):
@@ -352,7 +454,7 @@ class FilenameTemplator:
     replacements = {
           "%d" : lambda calc: datetime.today().strftime('%Y-%m-%d_%H:%M'),
           "%t" : lambda calc: calc.task.task_name if calc.task else 'sprkkr',
-          "%a" : lambda calc: str(self.atoms.symbols) if calc.atoms else 'custom',
+          "%a" : lambda calc: str(calc.atoms.symbols) if calc.atoms else 'custom',
           "%c" : lambda calc: calc.advance_counter()
         }
 
