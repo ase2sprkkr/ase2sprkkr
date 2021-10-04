0202e421e836a144c130dc5eb9746f3099e4fea6
Matyas Novak <logik@centrum.cz>
2021-10-04 10:24:55 +0200

    The requirements for the values in BASSCALE are relaxed

--- src/ase2sprkkr/potential/definitions/sections/sites.py
+++ src/ase2sprkkr/potential/definitions/sections/sites.py
@@ -34,7 +34,7 @@ class SitesSectionDefinition(PotSectionDefinition):
       V = PotValueDefinition
       members = [
           V('CARTESIAN', bool, fixed_value=True),
-          V('BASSCALE', Array(float, length=3), fixed_value=[1.,1.,1.]),
+          V('BASSCALE', Array(float, length=3), default_value=[1.,1.,1.]),
           V('SCALED_ATOMIC_POSITIONS', Table({'QBAS(X)': float, 'QBAS(Y)' : float, 'QBAS(Z)': float}, numbering='IQ',free_header=True)),
       ]
       super().__init__(name, members, has_hidden_members=True)
