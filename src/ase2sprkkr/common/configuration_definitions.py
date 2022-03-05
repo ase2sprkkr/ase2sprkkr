99dbaa2e0f53c1cca596062f6392f970d6370f41
Matyas Novak <logik@centrum.cz>
2022-03-05 23:31:31 +0100

    Pyparsing old/new version compatibility

--- src/ase2sprkkr/common/configuration_definitions.py
+++ src/ase2sprkkr/common/configuration_definitions.py
@@ -9,6 +9,10 @@ import inspect
 from .misc import cache
 import itertools
 
+_parse_all_name = 'parse_all' if \
+  'parse_all' in inspect.getfullargspec(pp.Or.parseString).args \
+  else 'parseAll'
+
 def unique_dict(values):
     out = dict(values)
     if len(values) != len(out):
@@ -507,10 +511,10 @@ class BaseDefinitionContainer(BaseDefinition):
        return not self._members.first_item().name_in_grammar
 
     def parse_file(self, file, return_value_only=True):
-        return self.parse_return(self.grammar().parseFile(file, parse_all=True), return_value_only)
+      return self.parse_return(self.grammar().parseFile(file, **{ _parse_all_name: True } ), return_value_only)
 
     def parse(self, str, whole_string=True, return_value_only=True):
-        return self.parse_return(self.grammar().parseString(str, parse_all=whole_string), return_value_only)
+      return self.parse_return(self.grammar().parseString(str, **{ _parse_all_name: whole_string} ), return_value_only)
 
     def parse_return(self, val, return_value_only=True):
         val = val[0]
