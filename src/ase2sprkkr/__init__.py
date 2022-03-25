9c8dc1c0da7a3680e88a3af083076c9a559649f1
Matyas Novak <logik@centrum.cz>
2022-03-25 10:40:40 +0100

    Shortcut imports

--- src/ase2sprkkr/__init__.py
+++ src/ase2sprkkr/__init__.py
@@ -1,3 +1,17 @@
 """
-ase2sprkkr - ASE interface to SPR-KKR electron structure calculation
+ase2sprkkr - ASE interface to SPR-KKR electron structure calculation.
+
+This root package import a few most used objects to provide shortcuts to them.
 """
+
+""" SPRKKR calculator to be used to calculate electronic structure using ASE """
+from .sprkkr.calculator import SPRKKR
+
+""" Input parameters object for SPRKKR calculation tasks """
+from .input_parameters.input_parameters import InputParameters
+
+""" Potential file object for SPRKKR calcualtions """
+from .potentials.potentials import Potential
+
+""" An extension of ASE atoms object """
+from .sprkkr.sprkkr_atoms import SPRKKRAtoms
