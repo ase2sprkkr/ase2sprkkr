42bf285a6afa57acc857ca56cbb0e25302f84e07
Matyas Novak <logik@centrum.cz>
2022-03-25 21:08:58 +0100

    Install dependencies

---
+++ install_dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python3 -m pip install `python <<EOF
+import configparser
+cp=configparser.ConfigParser()
+cp.read_file(open('setup.cfg', 'r'))
+print(cp['options']['install_requires'])
+EOF`
