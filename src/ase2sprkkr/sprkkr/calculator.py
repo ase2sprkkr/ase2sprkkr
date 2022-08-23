8c167809f0e9fa7933a336b57863adb560444e18
Matyas Novak <logik@centrum.cz>
2022-08-23 22:10:12 +0200

    Old Python compatibility

--- src/ase2sprkkr/sprkkr/calculator.py
+++ src/ase2sprkkr/sprkkr/calculator.py
@@ -10,6 +10,7 @@ For more informations about SPR-KKR, follow this
 view=article&id=8%3Asprkkr&catid=4%3Asoftware&Itemid=7&lang=en>`
 
 """
+from __future__ import annotations
 import os, sys
 from ase.calculators.calculator import Calculator, all_changes
 
