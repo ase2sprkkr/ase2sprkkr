184eee1c905cc545dcafaa80b071f38922fc9597
Matyas Novak <logik@centrum.cz>
2023-02-12 22:37:48 +0100

    No output inmprovements

--- src/ase2sprkkr/common/no_output.py
+++ src/ase2sprkkr/common/no_output.py
@@ -19,25 +19,32 @@ class NoOutput:
       """
       self.fd=fd if isinstance(fd, Iterable) else [ fd ]
       self.suppress = suppress
+      self.special = {
+          1: sys.stdout,
+          2: sys.stderr
+      }
 
   def __enter__(self):
       if not self.suppress: return
       self.null = open(os.devnull, "wb")
       null_fd = self.null.fileno()
-      special = {
-          1: sys.stdout,
-          2: sys.stderr
-      }
-
       def silence(fd):
+          try:
+            os.fsync(fd)
+          except OSError:
+            pass
           dup = os.dup(fd)
           s_dup = s_fd = None
-          if fd in special:
-             stream = special[fd]
+          if fd in self.special:
+             stream = self.special[fd]
              stream.flush()
              #pytest replaces the standard output
              if stream.fileno() != fd:
                 s_fd = stream.fileno()
+                try:
+                  os.fsync(s_fd)
+                except OSError:
+                  pass
                 s_dup = os.dup(s_fd)
                 os.dup2(null_fd, s_fd)
           os.dup2(null_fd, fd)
@@ -47,22 +54,27 @@ class NoOutput:
 
   def __exit__(self, type, value, traceback):
       if not self.suppress: return
-      special = {
-          1: sys.stdout,
-          2: sys.stderr
-      }
 
       itr=list(zip(self.fd, self.dups))
       itr.reverse()
 
       for fd, i in itr:
           dup, s_fd, s_dup = i
-          os.dup2(dup, fd)
-          os.close(dup)
-          if fd in special:
-             stream = special[fd]
+          if fd in self.special:
+             stream = self.special[fd]
              stream.flush()
              if s_fd:
+                try:
+                    os.fsync(s_fd)
+                except OSError:
+                    pass
                 os.dup2(s_dup, s_fd)
                 os.close(s_dup)
+
+          try:
+              os.fsync(fd)
+          except OSError:
+              pass
+          os.dup2(dup, fd)
+          os.close(dup)
       self.null.close()
