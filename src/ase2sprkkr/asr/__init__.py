3d176173767294c22d1351448546c31e7b328d28
Matyas Novak <logik@centrum.cz>
2023-06-08 09:42:23 +0200

    A bit of doc

--- src/ase2sprkkr/asr/__init__.py
+++ src/ase2sprkkr/asr/__init__.py
@@ -1,3 +1,11 @@
+""" This module just import ASR recipes.
+Thus, it allows to call them in the same way as the original ASR::
+
+  python3 -m ase2sprkkr.asr.relax --calculator "{'name':'sprkkr'}"
+
+However, the SPRKKR calculator is available for them.
+"""
+
 import asr
 __path__ = asr.__path__
 del asr
