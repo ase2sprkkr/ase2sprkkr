431c8218d452ad0378b97a6960949dee92959c1b
Matyas Novak <logik@centrum.cz>
2023-06-18 16:00:42 +0200

    ARPES results and test

---
+++ src/ase2sprkkr/outputs/readers/arpes.py
@@ -0,0 +1,28 @@
+""" The ARPES result: currently it """
+
+from ..task_result import TaskResult
+from .default import DefaultOutputReader
+from ...common.decorators import cached_property
+from ..output_files import OutputFile
+import os
+class ArpesResult(TaskResult):
+  """ Objects of this class holds the results of computed SCF class """
+
+  @cached_property
+  def spc_filename(self):
+      """ New (output) potential file name """
+      fname = self.input_parameters.CONTROL.DATASET() + '_ARPES_data.spc'
+      if self.directory:
+         fname = os.path.join(self.directory, fname)
+      return fname
+
+  @cached_property
+  def spc(self):
+      """ The new (output) potential - that contains the converged charge density etc. """
+      return OutputFile.from_file(self.spc_filename)
+
+class ArpesOutputReader(DefaultOutputReader):
+  """ ARPES task output reader currently do nothing, just have a special
+  result, that allow easy acces to spc output file """
+
+  result_class = ArpesResult
---
+++ src/ase2sprkkr/sprkkr/test/test_arpes.py
@@ -0,0 +1,47 @@
+if __package__:
+   from .init_tests import TestCase, patch_package
+else:
+   from init_tests import TestCase, patch_package
+__package__, __name__ = patch_package(__package__, __name__)
+
+from ..calculator import SPRKKR
+from ...potentials.potentials import Potential
+import os
+import sys
+import numpy as np
+from ase.atoms import Atoms
+
+class CalculatorTest(TestCase):
+
+ print_output = '-v' in sys.argv or '--verbose' in sys.argv
+ dirname = os.path.dirname(__file__)
+ _calc_args = dict(
+     directory = dirname, input_file = 'output_test_calc.inp', #empty_spheres=False,
+     output_file = 'output_test_calc.out', potential_file ='output_test_calc.pot', print_output=print_output,
+     mpi = 'auto'
+ )
+
+ @classmethod
+ def calc_args(cls, **kwargs):
+      kwargs.update(cls._calc_args)
+      return kwargs
+
+ def run_sprkkr(self):
+      return os.environ.get('DO_NOT_RUN_SPRKKR', '') == ''
+
+ def test_run(self):
+      if not self.run_sprkkr(): return
+
+      a=Atoms(symbols='H',cell=np.array([(1.,0,0),(0,1,0),(0,0,1)]))
+      a.pbc=True
+      xx=SPRKKR(atoms=a)
+      xx.input_parameters.SCF.NITER=1
+      out = xx.calculate()
+      calc = out.calculator
+      calc.change_task('arpes')
+      calc.set('NE', 5)
+      calc.set('NKTAB', 5)
+      calc.set('NKTAB2D', 5)
+      calc.set('NKTAB3D', 5)
+      out = out.calculator.calculate()
+      self.assertTrue(isinstance(out.spc.ENERGY(), np.ndarray))
