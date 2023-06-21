c8ddeb1e514a5259312b816d340fb6ff24e89da6
Matyas Novak <logik@centrum.cz>
2023-06-21 21:01:31 +0200

    More advanced parsing for NumpyArray grammar type

--- src/ase2sprkkr/common/grammar_types/data.py
+++ src/ase2sprkkr/common/grammar_types/data.py
@@ -25,9 +25,43 @@ class NumpyArray(GrammarType):
     array_access = True
 
     @add_to_signature(GrammarType.__init__)
-    def __init__(self, *args, delimiter=None, written_shape=None, **kwargs):
+    def __init__(self, *args, delimiter=None, written_shape=None, item_format=None, indented=False, **kwargs):
+        """
+        Parameters
+        ----------
+
+        delimiter
+          None - default behavior.
+          int  - the number will take given fixed number of chars
+
+        written_shape
+          resize to given shape before writing
+
+        indented
+          If the file has the following structure::
+
+             .......................................
+                  ....rest of the splitted line.....
+                  ....rest of the splitted line.
+             ....... The second line................
+                  ..... the rest of the ............
+                  ............ second line ...
+             ............
+
+          Pass a tuple with two integers into this argument.
+          The first number of tuple is the number of characters on on line max,
+          longer lines will be splitted.
+          The second number is the number of spaces placed on the begining of the
+          new lines created by splitting the old.
+
+        format
+          Format string used to write, e.g.`%.4e`
+
+        """
         self.delimiter=delimiter
         self.written_shape=written_shape
+        self.item_format=item_format
+        self.indented=indented
         super().__init__(*args, **kwargs)
 
     def _validate(self, value, why='set'):
@@ -43,15 +77,38 @@ class NumpyArray(GrammarType):
            delimiter = ''
        if self.written_shape:
           out=out.reshape(self.written_shape)
-       np.savetxt(out, value, delimiter=delimiter)
-       return np.savetxt(io)
+       np.savetxt(out, value, delimiter=delimiter, format=self.item_format)
+       np.savetxt(io)
+       out=io.getvalue()
+       if indented:
+          first = indented[0]
+          nexts = first - indented[1]
+          prefix = ' '*indented[1]
+          def g():
+              for i in '\n'.split(''):
+                  yield i[:first]
+                  s=firsts
+                  l=len(i)
+                  while s<l:
+                    e=firsts+nexts
+                    yield prefix+i[s:e]
+                    s=e
+          out = '\n'.join(g)
+       return out
+
 
     is_the_same_value = staticmethod(compare_numpy_values)
 
     def _grammar(self, param_name=False):
          out = RestOfTheFile._grammar.copy()
+         def parse(v):
+             if self.indented:
+                v=v.replace('\n'+' '*self.indented[1], '')
+             v=np.genfromtxt( io.StringIO(v), delimiter=self.delimiter )
+             return v
+
          out.setParseAction(
-                lambda v: np.genfromtxt( io.StringIO(v[0]), delimiter=self.delimiter )
+                lambda v: parse(v[0])
          )
          return out
 
