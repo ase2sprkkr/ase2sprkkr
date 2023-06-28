3ed0cec5441ea62fec28537ac7469ad824e1a0b6
Matyas Novak <logik@centrum.cz>
2023-06-28 22:17:17 +0200

    KRMT and KRWS docs

--- src/ase2sprkkr/input_parameters/definitions/scf.py
+++ src/ase2sprkkr/input_parameters/definitions/scf.py
@@ -8,8 +8,16 @@ from ..input_parameters_definitions import \
 input_parameters = InputParameters(
       'scf', [
         CONTROL('SCF').copy([
-          V('KRWS', Integer(min=0, max=1), 1),
-          V('KRMT', Integer(min=0, max=6), required=False)
+          V('KRWS', Integer(min=0, max=1), 1, info='If 0 RWS is taken from the potential file and scaled. If 1, RWS is calculated by scaling the muffin-tin radii by a common scaling factor. (This setting is forced in the case of FULLPOT.)'),
+          V('KRMT', {
+            '0' : 'RMT is taken from the potential file',
+            '1' : 'RMT = min( x*RWS )',
+            '2' : 'RMT = min( d_ij / 2 )',
+            '3' : 'RMT from atomic charge density (=> KRWS=1)',
+            '4' : 'RMT from atomic Hartree potential (=> KRWS=1)',
+            '5' : 'RMT from total atomic potential (=> KRWS=1)',
+            '6' : 'take average of 3 and 4 (=> KRWS=1)',
+          }, info='It controls how the muffin-tin radii are calculated.',required=False)
         ]),
         TAU,
         ENERGY,
