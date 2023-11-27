070333b1e894945fd78eaa052e5c0282692dd542
Matyas Novak <logik@centrum.cz>
2023-11-27 14:26:48 +0100

    Fixup output files

--- src/ase2sprkkr/output_files/test/test_files.py
+++ src/ase2sprkkr/output_files/test/test_files.py
@@ -1,23 +1,25 @@
+from os import listdir
+from os.path import dirname, join as path_join, isfile
+import numpy as np
+from ase.units import Rydberg as Ry
+import tempfile
+
+
 if __package__:
    from .init_tests import TestCase, patch_package
 else:
    from init_tests import TestCase, patch_package
 __package__, __name__ = patch_package(__package__, __name__)
 
-from os import listdir
-from os.path import dirname, join as path_join, isfile
-import os.path
-from ..output_files import OutputFile
+if True:
+    from ..output_files import OutputFile
+    from ...common.configuration_containers import DisabledAttributeError
 
-import scipy.constants
-from ase.units import Rydberg as Ry
-from ...common.configuration_containers import DisabledAttributeError
-import tempfile
 
 class TestOutput(TestCase):
 
   def test_output(self):
-    dire = path_join(dirname(dirname(__file__)), 'output_files', 'examples')
+    dire = path_join(dirname(dirname(__file__)), 'examples')
     for i in listdir(dire):
         fname = path_join(dire, i)
         if isfile(fname):
@@ -30,7 +32,7 @@ class TestOutput(TestCase):
                for i in 'ENERGY', 'THETA', 'K', 'DETERMINANT':
                    self.assertEqual(out[i](), o2[i]())
                for i in 'TOTAL', 'POLARIZATION', 'UP', 'DOWN':
-                   self.assertEqual(2*out[i](), o2[i]())
+                   self.assertEqual(2 * out[i](), o2[i]())
 
             elif ext=='dos':
                self.assertEqual(out.n_orbitals(1), 3)
