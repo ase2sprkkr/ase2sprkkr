f410dd2381b6b5f41594d1384bdb34a876056eac
Matyas Novak <logik@centrum.cz>
2024-06-08 19:32:52 +0200

    [Empty spheres] Subprocess SIGCHLD signal ignored

--- src/ase2sprkkr/common/subprocess.py
+++ src/ase2sprkkr/common/subprocess.py
@@ -1,20 +1,41 @@
 from concurrent.futures import ProcessPoolExecutor
 import importlib
+import signal
+import contextlib
+
 _pool=None
+
+
+@contextlib.contextmanager
+def ignore_signal(num):
+    old = signal.getsignal(num)
+    signal.signal(num, signal.SIG_IGN)
+    try:
+      yield
+    finally:
+      signal.signal(num, old)
 
 
 def _call_function(name, module, args, kwargs):
     mod = importlib.import_module(module)
-    return getattr(mod, name)(*args, **kwargs)
+    out = getattr(mod, name)(*args, **kwargs)
+    return out
 
 
 def in_subprocess(lmbda, module=None, *args, **kwargs):
     global _pool
+
     if not _pool:
        _pool = ProcessPoolExecutor(max_workers=1)
-    if module:
-        future = _pool.submit(_call_function, lmbda, module, *args, **kwargs)
-    else:
-        future = _pool.submit(lmbda)
+    try:
+        with ignore_signal(signal.SIGCHLD):
+            if module:
+                future = _pool.submit(_call_function, lmbda, module, *args, **kwargs)
+            else:
+                future = _pool.submit(lmbda)
+    except Exception as e:
+        _pool.shutdown(False)
+        _pool = None
+        raise RuntimeError("Error when running the supbprocess") from e
 
     return future.result()
