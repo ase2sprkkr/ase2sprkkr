133044a77f34f67c24759d4dd5771a2b9432678c
ridhaeddhib <110827259+ridhaeddhib@users.noreply.github.com>
2024-06-09 17:39:21 +0200

    Update sections.py
    Tiny issue

--- src/ase2sprkkr/input_parameters/definitions/sections.py
+++ src/ase2sprkkr/input_parameters/definitions/sections.py
@@ -135,7 +135,7 @@ SCF = Section('SCF', [
       V('ISTBRY', 1, info='Start Broyden after ISTBRY iterations'),
       V('FULLPOT', False, info='Non-spherical callculation (full-potential) instead of ASA'),
       V('ITDEPT', 40, info='Iteration depth for Broyden algorithm (length of used history)'),
-      V('QION', SetOf(float) required=False, info='Guess for the ionic charges Qt for atomic types'),
+      V('QION', SetOf(float), required=False, info='Guess for the ionic charges Qt for atomic types'),
       V('QIONSCL', float, required=False, info='Guess for the ionic charges Qt for atomic types'),
       V('MSPIN', SetOf(float), required=False, info='Guess for the magnetic moment μ_{spin,t} for atomic types'),
       V('USEVMATT', False, info='Set up the starting potential using the original Mattheiss'
