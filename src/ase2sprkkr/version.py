6033a3f0c7b3127b9ef8becaaf90d4742fee18cb
Matyas Novak <novakmat@fzu.cz>
2024-06-24 21:19:05 +0200

    Fix: version in non-installed environment

--- src/ase2sprkkr/version.py
+++ src/ase2sprkkr/version.py
@@ -6,4 +6,8 @@ Just the version of the ASE2SPRKKR package.
 """
 #: Version number of the ASE2SPRKKR package
 import pkg_resources
-__version__ = pkg_resources.get_distribution('ase2sprkkr').version
+
+try:
+    __version__ = pkg_resources.get_distribution('ase2sprkkr').version
+except pkg_resources.DistributionNotFound:
+    __version__ = None
