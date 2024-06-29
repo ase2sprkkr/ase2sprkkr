29a8bc44b30ce167dee12c460832dae54b4910a6
Matyas Novak <logik@centrum.cz>
2024-06-29 10:16:02 +0200

    [Build] Missing test files

--- src/sources.py
+++ src/sources.py
@@ -27,5 +27,8 @@ def sources(dir, ext):
 sources('ase2sprkkr', '.py')
 sources('ase2sprkkr/input_parameters/examples', '.in')
 sources('ase2sprkkr/output_files/examples', True)
+sources('ase2sprkkr/outputs/examples', '.out')
+sources('ase2sprkkr/bindings/xband/tests/', '.pot')
 sources('ase2sprkkr/output/examples', '.out')
+sources('ase2sprkkr/sprkkr/test', '.cif')
 sources('ase2sprkkr/potentials/examples', re.compile(r'.*[.](pot|new)'))
