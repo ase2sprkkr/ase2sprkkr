4d1067dc3ade5c73f7d5c4344323b7746c22317e
Matyas Novak <logik@centrum.cz>
2024-07-13 18:16:30 +0200

    Options in calculate method do not changes original input_parameters

--- src/ase2sprkkr/sprkkr/calculator.py
+++ src/ase2sprkkr/sprkkr/calculator.py
@@ -532,6 +532,8 @@ class SPRKKR(Calculator):
         def resolve_input_parameters(input_parameters, input_file, save_input):
             if input_parameters is None:
                 input_parameters=self.input_parameters
+            if task or options:
+                input_parameters = input_parameters.copy()
             if input_parameters:
                if isinstance(input_parameters, str):
                   if InputParameters.is_it_a_input_parameters_name(input_parameters):
