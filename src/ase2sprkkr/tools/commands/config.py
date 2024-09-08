0d9e35a10844d43eb72ef86f1b18480c05a978fc
Matyas Novak <logik@centrum.cz>
2024-09-08 17:24:08 +0200

    Better ase2sprkkr config command

--- src/ase2sprkkr/tools/commands/config.py
+++ src/ase2sprkkr/tools/commands/config.py
@@ -13,13 +13,33 @@ sys.path.append(root_path)
 
 from ...common.tools import main  # NOQA
 
-help='Show user configuration.'
+help='Show the (current) user configuration.'
 description='On the most modern unix/linux systems, it is in the file ~/.config/ase2sprkkr/__init__.py'
 
 
 def parser(parser):
     parser.add_argument('-p', '--path', help='Just print the path to the file.', action='store_true')
     parser.add_argument('-e', '--edit', help='Edit the file using the editor in the $EDITOR environment variable.', action='store_true')
+    parser.add_argument('-d', '--default', help='Put the default values into the file, if it not exists', action='store_true')
+    parser.add_argument('-D', '--show-default', help='Show the default values', action='store_true')
+
+
+def default_content(file):
+    return f""" # ASE2SRPKKR configuration file
+# -------------------------------
+
+# Do not comment the following line.
+from ase2sprkkr import config
+
+# This file is pure python and it is executed when ase2sprkkr is imported.
+# Place it into {file}
+
+# This string is appended to the runned executables
+# config.sprkkr_executable_suffix = ''
+
+# Uncomment, if you don't want to run empty-spheres finding by default
+# config.empty_spheres = False
+"""
 
 
 def run(args):
@@ -27,8 +47,14 @@ def run(args):
     from ...ase.register import user_preferences_file
     import subprocess
     file = user_preferences_file()
+
+    if args.default and not os.path.isfile(file):
+        with open(file, 'w') as f:
+            f.write(default_content(file))
     if args.path:
         print(file)
+    elif args.show_default:
+        print(default_content(file))
     elif args.edit:
         if 'EDITOR' not in os.environ:
             print("Please set the EDITOR environment variable.")
