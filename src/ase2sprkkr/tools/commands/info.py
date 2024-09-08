60460db9b18ca74a6038da68faaa9e3293bec728
Matyas Novak <logik@centrum.cz>
2024-09-08 16:48:43 +0200

    A better ase2sprkkr info

--- src/ase2sprkkr/tools/commands/info.py
+++ src/ase2sprkkr/tools/commands/info.py
@@ -15,11 +15,23 @@ from ...common.tools import main  # NOQA
 
 
 help='Show capabilities of ase2sprkkr (known tasks, output files formats etc.).'
-description='You can pass the input parameters using the command line. However, due to limitation of argparse, pass them directly after the potential file.'
+description="""\n Example of usage:
+    #show all tasks
+    ase2sprkkr info --task
+
+    #show information about task SCF
+    ase2sprkkr info --task=scf
+
+    #show information about section ENERGY in task SCF
+    ase2sprkkr info --task=scf.energy
+
+    #find all sections/values with name ENERGY in all tasks
+    ase2sprkkr info --task=.energy
+"""
 
 always_options = [
-  'tasks',
-  'output_files',
+  'task',
+  'output_file',
 ]
 
 possible_options = [] + always_options
@@ -56,15 +68,10 @@ class TestsArgAction(argparse.Action):
 
 
 def parser(parser):
-    parser.add_argument('about',
-                        nargs='*',
-                        help='About what do you want an information?',
-                        metavar=f'{{{",".join(possible_options)}}}',
-                        type=str,
-                        default=[],
-                        action=TestsArgAction
-                        )
-    parser.add_argument('--verbose', '-v', help='Give a more verbose info.', action='store_true')
+    parser.add_argument('--task', '-t', help='Show informations about the known tasks. You can limit the output to a given task/section/option by an optional agrument [TASK]', nargs='?', const=True)
+    parser.add_argument('--output-file', '-o', help='Show informations about the known output files. You can ', nargs='?', const=True)
+    action = getattr(argparse, 'BooleanOptionalAction', 'store_true')
+    parser.add_argument('--verbose', '-v', help='Give a more verbose info. It is set automatically, if the output is limited.', action=action, default=None)
 
 
 def run(args):
@@ -73,33 +80,91 @@ def run(args):
           "==========\n"
           "A tool to run SPRKKR calculations\n\n")
 
-    g = globals()
-    for i in always_options:
-        if not args.about or i in args.about:
-            g["info_" + i](args)
+    def info_task(args, filter):
+        input_parameters = InputParameters.definitions.values()
+        if filter is True:
+            input_parameters = list(input_parameters)
+            path = None
+            print("The known SPRKKR tasks (kind of input parameters)\n"
+                  "---------------------------------------------------")
+        else:
+            if '.' in filter:
+                filter,path = filter.split('.', 1)
+            input_parameters = [ i for i in input_parameters if filter in i.name.lower() ]
+            if path:
+                if len(input_parameters) == 0:
+                    print("There is no known SPRKKR task with '{filter.upper()}' in its name")
+                    return
+                if filter == '':
+                    tasks = 'all tasks'
+                elif len(input_parameters) == 1 and \
+                   input_parameters[0].name.upper() == filter.upper():
+                       tasks = f"the task '{input_parameters[0].name.upper()}'"
+                else:
+                    tasks = f"the tasks containing '{filter}'"
+                x=f"The configuration sections/values '{path.upper()}' in {tasks}"
+                print(f"{x}\n{'-'*len(x)}")
+            else:
+                x=f"The SPRKKR tasks containing '{filter}'"
+                print(f"{x}\n{'-'*len(x)}")
+
+        for i in input_parameters:
+            def print_ip(i, prefix='', add=None):
+                if args.verbose or args.verbose is None and filter is not True:
+                    print("\n")
+                    if add:
+                        print(add)
+                    print(i.description(prefix=prefix, verbose=True))
+                else:
+                    if add:
+                        print(add)
+                    print(prefix + i.info())
+
+            if path is not None:
+                ii = list(i.create_object().get_members(path))
+                if not ii:
+                  print(f"No such member of TASK {'' if filter is True else filter} named {path}")
+                for val in ii:
+                    val = val._definition
+                    add = f"{val.item_type.capitalize()} {val.get_path()} of task {i.name.upper()}:"
+                    print_ip(val, '  ', add)
+            else:
+                print_ip(i, prefix='')
+
+    def info_output_file(args, filter):
+        print("Known SPRKKR output files\n"
+              "--------------------")
+        filter = filter.lower()
+        for ext, i in OutputFile.definitions.items():
+            i = i.definition
+            if filter is not True and (
+                (filter not in ext.lower()) or
+                (filter not in i.name.lower())
+              ):
+                continue
+
+            if args.verbose:
+                print("\n\n")
+            print(f"{ext.upper()}: ",end='')
+
+            if args.verbose or args.verbose is None and filter is not True:
+                print(i.description(verbose=True))
+            else:
+                print(i.info())
+
+    g = locals()
+
+    def print_option(i, filter):
+        if filter is not True:
+            filter = filter.lower()
+        g["info_" + i](args, filter)
         print("\n")
 
-
-def info_tasks(args):
-    print("Possible task (kind of input parameters)\n"
-          "----------------------------------------")
-    for i in InputParameters.definitions.values():
-        if args.verbose:
-            print("\n\n")
-            print(i.description(verbose=True))
-        else:
-            print(i.info())
-
-
-def info_output_files(args):
-    print("Known output files\n"
-          "--------------------")
-    for ext, i in OutputFile.definitions.items():
-        if args.verbose:
-            print("\n\n")
-        print(f"{ext.upper()}: ",end='')
-        i = i.definition
-        if args.verbose:
-            print(i.description(verbose=True))
-        else:
-            print(i.info())
+    printed=False
+    for i in possible_options:
+        if getattr(args, i) is not None:
+            print_option(i, getattr(args, i))
+            printed=True
+    if not printed:
+        for i in always_options:
+            print_option(i, True)
