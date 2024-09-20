0a1d164326c55a16490503143e69ffe1c8e086a9
Matyas Novak <logik@centrum.cz>
2024-09-20 10:19:02 +0200

    Run can accept input_file itself as well.

--- src/ase2sprkkr/tools/commands/run.py
+++ src/ase2sprkkr/tools/commands/run.py
@@ -10,8 +10,8 @@ if not __package__:
   __package__ = 'ase2sprkkr.tools.commands'
 sys.path.append(str(Path(__file__).resolve().parents[3]))
 
-from ... import InputParameters, SPRKKR, Potential  # NOQA
 from ...common.tools import parse_named_option, main  # NOQA
+from ... import InputParameters  # NOQA
 
 
 help='Run SPRKKR calculation.'
@@ -20,26 +20,46 @@ description='You can pass the input parameters using the command line. However, 
 
 def parser(parser):
     bool_arg = getattr(argparse, 'BooleanOptionalAction', 'store_true')
-    parser.add_argument('pot', help='SPR-KKR potential file')
-    parser.add_argument('-t','--task', dest='task', choices=InputParameters.definitions.keys(), type = str.lower, help='Task to compute')
-    parser.add_argument('--print-output', '-O', help='Print the output of SPRKKR', default='None', action=bool_arg)
+    parser.add_argument('pot', help='SPR-KKR potential file or input file. The type is determined by its content.')
+
+    grp = parser.add_mutually_exclusive_group()
+    grp.add_argument('-t','--task', dest='task', choices=InputParameters.definitions.keys(), type = str.lower, help='Task to compute.')
+    grp.add_argument('--input-file', '-i', help='Run ase2sprkkr -i inputfile potfile, if you want to run task determined by the give input file with a given potential.', type=str)
+
+    parser.add_argument('--print-output', '-O', help='Print the output of SPRKKR.', default='None', action=bool_arg)
     parser.add_argument('--output-file', '-o', help='Output file', type=str)
-    parser.add_argument('--input-file', '-i', help='Input (parameters) file', type=str)
-    parser.add_argument('--empty-spheres', '-e', help='Try to add empty spheres (by default, they are added if the potential is not converged and no vacuum atom is present', action=bool_arg)
-    parser.add_argument('options', nargs='*', help='Input parameters in the form <name>=<value>', type=parse_named_option)
+    parser.add_argument('--empty-spheres', '-e', help='Try to add empty spheres (by default, they are added if the potential is not converged and no vacuum atom is present.', action=bool_arg)
+    parser.add_argument('options', nargs='*', help='Input parameters in the form <name>=<value>.', type=parse_named_option)
 
 
 def run(args):
+    from ... import SPRKKR, Potential  # NOQA
+    import pyparsing
+    import sys
+
     calc = SPRKKR()
-    if args.task:
+    try:
+        potential=Potential.from_file(args.pot)
+    except pyparsing.ParseException:
+        try:
+            calc.input_parameters = ip = InputParameters.from_file(args.pot)
+            potential = ip.CONTROL.POTFIL()
+        except pyparsing.ParseException:
+            print(f"File {args.pot} is neither potential nor input_parameters file")
+            sys.exit(-1)
+
+    if args.input_file:
+        calc.input_parameters = args.input_file
+    elif args.task:
         calc.input_parameters = args.task
     kwargs = {}
     for i in 'empty_spheres', 'print_output', 'output_file':
         o=getattr(args, i)
         if o is not None:
             kwargs[i]=o
-    calc.calculate(potential=args.pot,
-                   input_parameters=args.input_file,
+
+    calc.calculate(potential=potential,
+                   input_parameters=ip,
                    options=dict(args.options), **kwargs)
 
 
