a6b54974fd6e8bdc60658547b26e8f2d6bc5413e
Matyas Novak <logik@centrum.cz>
2024-09-27 23:42:50 +0200

    Drop python 3.7 and old numpy and pyparsing compatibility.

--- src/ase2sprkkr/common/backward_compatibility.py
+++ src/ase2sprkkr/common/backward_compatibility.py
@@ -1,18 +1,14 @@
 """ In this module, backward compatibility issues are solved by mocking """
 import functools
 import enum
-import pyparsing as pp
 from . import decorators
-
-if not hasattr(pp.ParserElement, "setName"):
-    pp.ParserElement.setName = getattr(pp.ParserElement, "set_name", lambda x: None)
 
 if not hasattr(functools,'cache'):
     functools.cache = functools.lru_cache(maxsize=None)
     """ Functools.cache. Python 3.8 and earlier does not have this method, so it is mocked for this version of python. """
 
 if not hasattr(enum, 'nonmember'):
-    """ I hope it will be working """
+
     def _nonmember(cls):
         def fn(self):
              return cls
