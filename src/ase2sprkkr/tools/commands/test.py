40b1db0a9fd6b9dfd8a96fd5d63baab4b4d8d4a7
Matyas Novak <logik@centrum.cz>
2024-09-27 22:12:53 +0200

    Return value for tests

--- src/ase2sprkkr/tools/commands/test.py
+++ src/ase2sprkkr/tools/commands/test.py
@@ -38,8 +38,9 @@ def run(args):
     if pytest.version_tuple[0] <= 6:
         print("Pytest version >= 6.0 is required. Please install it with pip install --upgrade 'pytest>=6'")
         exit(-1)
-    subprocess.run([sys.executable, '-m', 'pytest', '--import-mode=importlib', '--doctest-modules', a2s_path] +
+    out = subprocess.run([sys.executable, '-m', 'pytest', '--import-mode=importlib', '--doctest-modules', a2s_path] +
                    args.pytest_arguments)
+    exit(out.returncode)
 
 
 if __name__ == "__main__":
