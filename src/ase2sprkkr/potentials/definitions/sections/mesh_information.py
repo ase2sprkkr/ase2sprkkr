cbf171d7d91f12eedd09af1f3c6610cf191ed9ad
Matyas Novak <logik@centrum.cz>
2024-10-08 23:46:26 +0200

    Lint

--- src/ase2sprkkr/potentials/definitions/sections/mesh_information.py
+++ src/ase2sprkkr/potentials/definitions/sections/mesh_information.py
@@ -2,12 +2,14 @@ from ...potential_definitions import PotSectionDefinition, \
                                    PotValueDefinition
 from ...potential_sections import UniqueListSection
 
-from ....common.grammar_types import DefKeyword, Array, Table, Integer
+from ....common.grammar_types import DefKeyword, Table
 from ....sprkkr.radial_meshes import ExponentialMesh
+
 
 class MeshInformationSection(UniqueListSection):
   _value_name = 'meshes'
   _value_class = ExponentialMesh
+
 
 class MeshInformationSectionDefinition(PotSectionDefinition):
 
@@ -16,12 +18,13 @@ class MeshInformationSectionDefinition(PotSectionDefinition):
       members = [
           V('MESH-TYPE', DefKeyword('EXPONENTIAL')),
           V('DATA', Table({'R(1)': 1e-6, 'DX' :2e-2, 'JRMT': 0, 'RMT': 0., 'JRWS': 721, 'RWS': 0.},
-                    numbering='IM', numbering_format='>5', format='>16', #header_length=80
+                    numbering='IM', numbering_format='>5', format='>16',  # header_length=80
                           )
             ),
       ]
       super().__init__(name, members, has_hidden_members=True)
 
   result_class = MeshInformationSection
+
 
 section = MeshInformationSectionDefinition
