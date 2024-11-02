5daed4511de2b87386ab85afdbf0f1bb6fa55948
Matyas Novak <logik@centrum.cz>
2024-11-02 18:24:42 +0100

    Lint

--- src/ase2sprkkr/potentials/definitions/sections/global_system_parameter.py
+++ src/ase2sprkkr/potentials/definitions/sections/global_system_parameter.py
@@ -2,12 +2,14 @@ from ...potential_definitions import PotSectionDefinition, \
                                    PotValueDefinition
 from ...potential_sections import PotentialSection
 
+
 class GlobalSystemParameter(PotentialSection):
 
   def _set_from_atoms(self, atoms, write_io_data):
       self['NQ'].set(len(atoms.sites))
       self['NT'].set(write_io_data.types.len_of_unique())
       self['NM'].set(write_io_data.meshes.len_of_unique())
+
 
 class GlobalSystemParameterDefinition(PotSectionDefinition):
 
@@ -23,5 +25,6 @@ class GlobalSystemParameterDefinition(PotSectionDefinition):
       super().__init__(name, members, has_hidden_members=True)
 
   result_class = GlobalSystemParameter
+
 
 section = GlobalSystemParameterDefinition
