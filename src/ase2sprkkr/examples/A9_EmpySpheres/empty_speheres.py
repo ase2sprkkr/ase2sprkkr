de9a6ca9df2558e7fadb98c0892ed2e0fabd405d
Matyas Novak <logik@centrum.cz>
2024-11-07 20:29:08 +0100

    Fix of tests

--- src/ase2sprkkr/examples/A9_EmpySpheres/empty_speheres.py
+++ src/ase2sprkkr/examples/A9_EmpySpheres/empty_speheres.py
@@ -1,18 +1,24 @@
-from ase.build import bulk
-atoms=bulk('Cu','fcc', a=3.6, orthorhombic=True)
-atoms = atoms[:1]
+def main():
 
-from ase2sprkkr import SPRKKR
-calc=SPRKKR()
+    from ase.build import bulk
+    from ase2sprkkr import SPRKKR
 
-#default values
-#out = calc.calculate(atoms=atoms, options={'NITER': 1 }, empty_spheres=True)
+    atoms=bulk('Cu','fcc', a=3.6, orthorhombic=True)
+    atoms = atoms[:1]
 
-#min and max radius
-#out = calc.calculate(atoms=atoms, options={'NITER': 1 }, empty_spheres=(0.5, 1.5))
+    calc=SPRKKR()
 
-#custom arguments of ase2sprkkr.bindings.empty_spheres, resulting either in calling
-#es_finder, if it is available, or xband empty_spheres routines, called from the function
-#empty_spheres in ase2sprkkr.bindings.xband.spheres (compiled from spheres.pyx)
-out = calc.calculate(atoms=atoms, options={'NITER': 1 }, empty_spheres={'max_radius': 2., 'verbose' : True, 'max_spheres': 300})
-print(atoms.positions)
+    #default values
+    #out = calc.calculate(atoms=atoms, options={'NITER': 1 }, empty_spheres=True)
+
+    #min and max radius
+    #out = calc.calculate(atoms=atoms, options={'NITER': 1 }, empty_spheres=(0.5, 1.5))
+
+    #custom arguments of ase2sprkkr.bindings.empty_spheres, resulting either in calling
+    #es_finder, if it is available, or xband empty_spheres routines, called from the function
+    #empty_spheres in ase2sprkkr.bindings.xband.spheres (compiled from spheres.pyx)
+    out = calc.calculate(atoms=atoms, options={'NITER': 1 }, empty_spheres={'max_radius': 2., 'verbose' : True, 'max_spheres': 300})
+    print(atoms.positions)
+
+if __name__ == '__main__':
+    main()
