60308b6ec4e8b00c0d3f7998463d916e5b241c74
Matyas Novak <logik@centrum.cz>
2025-03-01 20:53:54 +0100

    test

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 python3 -m pip install git+https://github.com/lokik/spglib.git
 python3 -m pip install . --verbose
+python3 << EOF
+import numpy
+print(numpy.__version__)
+print(numpy.__path__)
+EOF
