fb4a08f980adbc239b321e37e357f7e327c6c7a9
Matyas Novak <logik@centrum.cz>
2025-09-22 22:16:30 +0200

    test_spec_parser

--- src/ase2sprkkr/outputs/test/test_spec_parser.py
+++ src/ase2sprkkr/outputs/test/test_spec_parser.py
@@ -1,15 +1,27 @@
 # Test the SpecResult class without loading a file
 from ase2sprkkr.outputs.readers.spec import SpecResult
+import numpy as np
 
-# Create a test class that inherits from SpecResult
-class TestSpecResult(SpecResult):
-    def __init__(self, content):
-        self._file_content = content
-        self.output_file = None  # Needed by parent class
+if __package__:
+   from .init_tests import TestCase, patch_package
+else:
+   from init_tests import TestCase, patch_package
+__package__, __name__ = patch_package(__package__, __name__)
 
-# Sample content that matches the format of spec.out
-test_content = """
-parameter for potential barrier:
+
+class TestSpecResult(TestCase):
+
+  def test_result(self):
+        # Create a test class that inherits from SpecResult
+        class TestSpecResult(SpecResult):
+            def __init__(self, content):
+                self._file_content = content
+                self.output_file = None  # Needed by parent class
+
+
+        # Sample content that matches the format of spec.out
+        test_content = """
+ parameter for potential barrier:
 # ibar:  1
 # epsx: 0.5000000E-02
 # zparup: 0.0000000E+00 -0.1000000E+01 0.1000000E+01
@@ -32,16 +44,21 @@ stookes-vector    of the photonfield  0.10000E+01  100.000%    0.000%    0.000%
 1.9000E+00  -9.6825E-02  5.7837E-08 -4.2368E-04  2.8906E-08  2.8931E-08
 """
 
-# Create a test instance
-test_result = TestSpecResult(test_content)
+        # Create a test instance
+        out = TestSpecResult(test_content)
 
-# Test the properties
-print("Testing potential_barrier:")
-print(test_result.potential_barrier)
-print("\nTesting lattice_constants:")
-print(test_result.lattice_constants)
-print("\nTesting basis_vectors:")
-for name, vector in test_result.basis_vectors.items():
-    print(f"{name}: {vector}")
-print("\nTesting spectral_data:")
-print(test_result.spectral_data)
+        # Test the properties
+        # print("Testing potential_barrier:")
+        self.assertEqual(out.potential_barrier,
+{'ibar': 1, 'epsx': 0.005, 'zparup': [0.0, -1.0, 1.0], 'zpardn': [0.0, -1.0, 1.0], 'bparp': [1.34, -2.0, 1.018]})
+        self.assertEqual(out.lattice_constants, {'a': 4.62983});
+        self.assertEqual(out.basis_vectors['real'],
+              np.asarray( [[4.62983, 0.     ],[-2.31492,  4.00955]] )
+        )
+        self.assertEqual(out.basis_vectors['reciprocal'],
+              np.asarray( [[1.35711, 0.78353], [0.     , 1.56706]] )
+        )
+        self.assertEqual(out.spectral_data,
+            np.asarray([[ 1.8937e+00, -9.6825e-02,  6.1567e-08, -3.9380e-04,  3.0771e-08, 3.0795e-08],
+                       [ 1.9000e+00, -9.6825e-02,  5.7837e-08, -4.2368e-04,  2.8906e-08, 2.8931e-08]])
+        )
