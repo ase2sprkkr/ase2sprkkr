047223aedc0e06ba678ae93c8368572a13ab38fe
Matyas Novak <logik@centrum.cz>
2026-03-03 16:55:35 +0100

    Mendeelev fix that allows to use pip

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,7 +3,7 @@ PYTHON_MINOR=$(python3 -c 'import sys; print(sys.version_info.minor)')
 
 if (( PYTHON_MINOR >= 14 )); then
     echo "Python >= 3.14 detected. Installing monty 2025.3.3 ignoring Python version metadata..."
-    pip install --ignore-requires-python monty==2025.3.3 pymatgen==2025.10.7
+    pip install --ignore-requires-python monty==2025.3.3 pymatgen==2025.10.7 mendeleev==1.1.0
 fi
 
 python3 -m pip install git+https://github.com/lokik/spglib.git
