b8d54dd1446094cdee2825235287794cb4eafb77
Matyas Novak <logik@centrum.cz>
2026-03-23 00:48:57 +0100

    Better naming

--- src/ase2sprkkr/sprkkr/moments.py
+++ src/ase2sprkkr/sprkkr/moments.py
@@ -1,13 +1,17 @@
 import copy
 
 class Moments:
-
+    #QEL  NOS  SPN  ORB  HFI
     def __init__(self, qel, nos, smt, omt, hff):
         self.qel = qel
         self.nos = nos
         self.smt = smt
         self.omt = omt
         self.hff = hff
+
+    @property
+    def spin_moment(self):
+        return self.smt
 
     def as_tuple(self):
         return (self.qel, self. nos, self. smt, self. omt, self. hff)
