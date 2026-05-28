3dfc3bac7b2917fb8b87b4b16d6529b624f87ad7
Matyas Novak <logik@centrum.cz>
2026-05-28 23:59:28 +0200

    Test for lattice data

--- src/ase2sprkkr/sprkkr/test/test_sysfile.py
+++ src/ase2sprkkr/sprkkr/test/test_sysfile.py
@@ -9,9 +9,53 @@ else:
 __package__, __name__ = patch_package(__package__, __name__)
 
 from ..sysfile import sysfile_content, write_sysfile  # NOQA: E402
+from ...physics.lattice_data import ap_number_from_spacegroup  # NOQA: E402
 
 
 class TestSysfile(TestCase):
+    def test_ap_number_from_spacegroup(self):
+        self.assertEqual(ap_number_from_spacegroup(3, 3), 4)
+        self.assertEqual(ap_number_from_spacegroup(3, 4), 3)
+        self.assertEqual(ap_number_from_spacegroup(3, 5), 3)
+
+        self.assertEqual(ap_number_from_spacegroup(5, 9), 10)
+        self.assertEqual(ap_number_from_spacegroup(5, 12), 8)
+        self.assertEqual(ap_number_from_spacegroup(5, 16), 7)
+
+        self.assertEqual(ap_number_from_spacegroup(7, 21), 18)
+        self.assertEqual(ap_number_from_spacegroup(7, 24), 16)
+        self.assertEqual(ap_number_from_spacegroup(7, 29), 15)
+
+        self.assertEqual(ap_number_from_spacegroup(9, 39), 33)
+        self.assertEqual(ap_number_from_spacegroup(9, 45), 29)
+        self.assertEqual(ap_number_from_spacegroup(9, 54), 27)
+
+        self.assertEqual(ap_number_from_spacegroup(136, 419), 384)
+
+        self.assertEqual(
+            ap_number_from_spacegroup(
+                143, cell=Cell.fromcellpar([3, 3, 5, 90, 90, 120])
+            ),
+            395,
+        )
+        self.assertEqual(
+            ap_number_from_spacegroup(
+                143, cell=Cell.fromcellpar([3, 3, 3, 75, 75, 75])
+            ),
+            396,
+        )
+
+        self.assertEqual(ap_number_from_spacegroup(146, 433), 402)
+        self.assertEqual(ap_number_from_spacegroup(146, 434), 401)
+
+        self.assertEqual(ap_number_from_spacegroup(160, 450), 420)
+        self.assertEqual(ap_number_from_spacegroup(160, 451), 419)
+
+        self.assertEqual(ap_number_from_spacegroup(166, 458), 428)
+        self.assertEqual(ap_number_from_spacegroup(166, 459), 427)
+
+        self.assertEqual(ap_number_from_spacegroup(136), 384)
+
     def test(self):
         a = 4.6
         c = 2.95
