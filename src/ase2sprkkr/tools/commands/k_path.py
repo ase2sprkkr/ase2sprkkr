70b041c426808a6b9d3ea23feffa8fbe031a81c1
Matyas Novak <logik@centrum.cz>
2026-05-29 11:39:05 +0200

    Python-like output of k-path tool

--- src/ase2sprkkr/tools/commands/k_path.py
+++ src/ase2sprkkr/tools/commands/k_path.py
@@ -12,9 +12,15 @@ sys.path.append(str(Path(__file__).resolve().parents[3]))
 
 from ...common.tools import main  # NOQA
 
-help = "GUI for specifiing BSF k-points PATH"
+help = "GUI for specifiing BSF k-points PATH (for E-K mode)"
 
-description = """\n Editor, where you can choose the points, along them is the k-path generated."""
+description = """\n Editor, where you can choose the points, along them is the k-path generated. The output can be added to the input parameters, e.g.
+
+from numpy import array
+calculator.input_parameters.set(
+{'NKDIR': 1, 'NK': 146, 'KA': array([[ 0.33333333, -0.57735027, -0.02551552]]), 'KE': array([[ 6.66666667e-01, -8.21078625e-17, -2.55155182e-02]])}
+)
+"""
 
 
 def parser(parser):
@@ -22,6 +28,9 @@ def parser(parser):
     parser.add_argument("potential", help="Potential file.", type=str)
     parser.add_argument(
         "-v", "--verbose", help="Verbose.", action="store_true", default=False
+    )
+    parser.add_argument(
+        "-i", "--input-file", help="Print the path as part of the SPRKKR input file."
     )
 
 
@@ -31,7 +40,10 @@ def run(args, global_args):
 
     kpath = k_path_gui(Potential.from_file(args.potential).atoms, verbose=args.verbose)
     if kpath:
-        print(k_path_to_string(kpath))
+        if args.input_file:
+            print(k_path_to_string(kpath))
+        else:
+            print(kpath)
     else:
         print("No k-path selected")
         exit(-1)
