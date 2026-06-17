b1731e80dbd9cc554b98b766c828d4e721445e62
Matyas Novak <novakmat@fzu.cz>
2026-06-17 11:46:30 +0200

    Fix of the test and a new test

--- src/ase2sprkkr/sprkkr/test/test_occupation.py
+++ src/ase2sprkkr/sprkkr/test/test_occupation.py
@@ -1,4 +1,6 @@
 import os
+import pytest
+from ase.build import bulk
 
 if __package__:
     from .init_tests import TestCase, patch_package
@@ -6,9 +8,8 @@ else:
     from init_tests import TestCase, patch_package
 __package__, __name__ = patch_package(__package__, __name__)
 
-
-class test(TestCase):
-    def test(self):
+class Test(TestCase):
+    def test_occ(self):
         from ase import io
         from ase2sprkkr.sprkkr.sprkkr_atoms import SPRKKRAtoms
 
@@ -17,5 +18,12 @@ class test(TestCase):
         SPRKKRAtoms.promote_ase_atoms(atoms)
         self.assertEqual(atoms.sites[0].occupation["Hf"], 0.7025)
         self.assertEqual(atoms.sites[0].occupation["Ti"], 0.2975)
-        self.assertEqual(atoms.sites[7].occupation["H"], 0.233)
-        self.assertEqual(atoms.sites[7].occupation["H"], 0.233)
+        self.assertEqual(atoms.sites[-1].occupation["H"], 0.233)
+
+        atoms = bulk('Fe', 'bcc', a=2.87, cubic=True)
+        SPRKKRAtoms.promote_ase_atoms(atoms)
+        with pytest.raises(RuntimeError):
+           atoms.sites[0].occupation['Fe'] = 0.5
+        atoms.sites[0].site_type.occupation['Fe'] = 0.5
+        atoms.sites[0].break_symmetry()
+        atoms.sites[0].occupation['Fe'] = 0.5
