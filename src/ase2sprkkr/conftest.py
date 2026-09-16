d6d7ea70536c00fd3150c3a10521cfbc0eabd934
Matyas Novak <novakmat@fzu.cz>
2026-09-16 09:57:45 +0200

    Ordering of the tests (slow are runned at the end)

---
+++ src/ase2sprkkr/conftest.py
@@ -0,0 +1,6 @@
+# pytest sorting stuff to run the fast tests first
+
+def pytest_collection_modifyitems(items):
+    items.sort(
+        key=lambda item: item.get_closest_marker("slow") is not None
+    )
